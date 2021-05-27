bf150fb9b2348543cd1cfa9a67db74156e08b5d6
Vectivus <54481654+Vectivuss@users.noreply.github.com>
2021-05-27 12:58:16 +0100

    Update cl_lib.lua

--- lua/autorun/client/cl_lib.lua
+++ lua/autorun/client/cl_lib.lua
@@ -10,6 +10,21 @@ function PlaySound( s )
 	surface.PlaySound( "vgui/" .. s .. ".ogg" )
 end
 
+function draw.BlurPanel( panel, amount, color )
+	local x, y = panel:LocalToScreen(0, 0)
+	local w, h = ScrW(), ScrH()
+	surface.SetDrawColor(255, 255, 255)
+	surface.SetMaterial(blur)
+	amount = amount or 4
+	for i = 1, 5 do
+		blur:SetFloat("$blur", (i / 3) * amount)
+		blur:Recompute()
+		render.UpdateScreenEffectTexture()
+		surface.DrawTexturedRect( x*-1, y*-1, w, h )
+	end
+	if color then draw.RoundedBox( 0, 0, 0, w, h, color ) end
+end
+
 function addMaterial( m, t )
 	surface.SetMaterial( Material( m, t ) )
 end
